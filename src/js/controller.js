9edc739ce51fe6ea4eef607658cb0ae2f11be0f2
hitpoint6 <hitpoint6@gmail.com>
2021-01-12 10:14:51 -0800

    Add new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -126,6 +126,10 @@ const controlAddRecipe = async function(newRecipe) {
   }
 }
 
+const newFeature = function (){
+  console.log('Welcome to the application.');
+}
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipe);
@@ -134,8 +138,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log("welcome!");
-
+  newFeature();
 };
-
 init();
