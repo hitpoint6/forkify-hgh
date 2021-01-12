7a6b326b98c76169c41ff364cf3e117f3edd76f2
hitpoint6 <hitpoint6@gmail.com>
2021-01-12 10:07:11 -0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
+export const KEY = 'fbc2ad98-32b3-4a0c-baa0-32bd265946b6';
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,15 @@
+import icons from 'url:../../img/icons.svg';
+import previewView from './previewView.js';
+import View from './View.js';
+
+class ResultsView extends View {
+    _parentElement = document.querySelector('.results');
+    _errorMessage = 'No recipe found.';
+    _message = '';
+
+    _generateMarkup() {
+      return this._data.map(result => previewView.render(result, false)).join('');
+    }
+}
+
+export default new ResultsView();
